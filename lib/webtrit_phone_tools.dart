5555bba1a1ae5d79e988e6571ee386984d621cbd
Yurii Cherniavskyi <ych@webtrit.com>
2023-11-17 14:06:43 +0200

    feat: initialize WebTrit Phone CLI tools
    To create Dart CLI application following command was executed:
    $ very_good create dart_cli webtrit_phone_tools --desc "WebTrit Phone CLI tools"

---
+++ lib/webtrit_phone_tools.dart
@@ -0,0 +1,10 @@
+/// webtrit_phone_tools, WebTrit Phone CLI tools
+///
+/// ```sh
+/// # activate webtrit_phone_tools
+/// dart pub global activate webtrit_phone_tools
+///
+/// # see usage
+/// webtrit_phone_tools --help
+/// ```
+library webtrit_phone_tools;
