481cc818056787f44e4197698cc6d5089cf6fa15
Dmitro Serdun <d.serdun@webtrit.com>
2024-05-12 16:08:42 +0300

    style: order commands export

--- lib/src/commands/commands.dart
+++ lib/src/commands/commands.dart
@@ -1,5 +1,5 @@
-export 'configurator_get_resources_command.dart';
 export 'configurator_generate_command.dart';
+export 'configurator_get_resources_command.dart';
 export 'keystore_commit_command.dart';
 export 'keystore_generate_command.dart';
 export 'update_command.dart';
